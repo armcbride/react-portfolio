898a4f803ff143bc08a8b14da699146aef8f453b
Annaruth McBride <armcbride17@gmail.com>
2020-06-27 02:50:49 -0500

    footer and nav bar connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
-import Home from "./pages/Home";
+// import Contact from "./pages/Contact";
+// import Projects from "./pages/Projects";
+// import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import './App.css';
 
@@ -13,9 +14,9 @@ function App() {
      <div>
      <Navbar />
       <Wrapper>
-        <Route exact path="/" component={Home}/>
+        {/* <Route exact path="/" component={Home}/>
         <Route exact path="/contact" component={Contact}/>
-        <Route exact path="/projects" component={Projects}/>
+        <Route exact path="/projects" component={Projects}/> */}
       </Wrapper>
       <Footer/>
      </div>
